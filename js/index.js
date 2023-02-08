5d8adc2605cf70116b2809eb1d6139a9e8d1ee05
tay-lamb <111663901+tay-lamb@users.noreply.github.com>
2023-02-07 20:06:32 -0500

    changes

---
+++ js/index.js
@@ -0,0 +1,11 @@
+console.log("your file is loaded correctly!")
+
+$("#flip").click(function(){
+    $("#panel").slideToggle();
+});
+$("#flip2").click(function(){
+    $("#panel2").slideToggle();
+});
+$("#flip3").click(function(){
+    $("#panel3").slideToggle();
+});
